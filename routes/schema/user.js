8a03919d09d406f8d8b00a33a38fbecae47910e0
Administrator <cunkuan.ding@tenbent.com>
2017-10-13 18:44:17 +0800

    引入bcrypt进行用户密码加密

--- routes/schema/user.js
+++ routes/schema/user.js
@@ -6,11 +6,30 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var db = require('../mongoose/db');
 
+var bcrypt = require('bcrypt');
+const saltRounds = 16;
+
 var User = new Schema({
     username: {type: String},
     password: {type: String},
     createTime: {type: Date, default: Date.now}
 });
+
+
+// 使用pre中间件在用户信息存储前进行密码加密
+User.pre('save', function (next) {
+    const that = this;
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(that.password, salt, function(err, hash) {
+            if (err) {
+                return next(err);
+            }
+            that.password = hash;
+            next();
+        });
+    });
+});
+
 
 // 编译模型
 module.exports = db.nodeMobile.model('User', User);
