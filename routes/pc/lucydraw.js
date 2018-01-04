bdf8c41f7a778a6ea89d381954b1a92fe0ecff65
Administrator <cunkuan.ding@tenbent.com>
2018-01-04 15:28:27 +0800

    auth user信息

--- routes/pc/lucydraw.js
+++ routes/pc/lucydraw.js
@@ -19,10 +19,74 @@ exports.pcRouter = function (router) {
             } else {
                 res.json({
                     code: '0',
-                    data: result
+                    result: result
                 });
             }
         })
+    });
+
+    /**
+     * 抽奖列表
+     */
+    router.get('/lucydrawlist', passport.authenticate('bearerPC', {session: false}), function (req, res, next) {
+        var page = req.query.page || 0;
+        var size = req.query.size || 10;
+        var query = LucyDraw.find({});
+        query.limit(parseInt(page));
+        query.skip(parseInt(page) * parseInt(size));
+        query.sort({createTime: 1});
+        query.exec(function (err, result) {
+            if (err) {
+                res.json({
+                    code: '500',
+                    msg: "Error: 服务器错误"
+                });
+            } else {
+                res.json({
+                    code: '0',
+                    result: result
+                });
+            }
+        });
+    });
+
+    /**
+     * 抽奖查询
+     */
+    router.get('/lucydraw', passport.authenticate('bearerPC', {session: false}), function (req, res, next) {
+        var id = req.query.id;
+        LucyDraw.findOne({_id:id},function (err,result) {
+            if (err) {
+                res.json({
+                    code: '500',
+                    msg: "Error: 服务器错误"
+                });
+            } else {
+                res.json({
+                    code: '0',
+                    result: result
+                });
+            }
+        });
+    });
+
+
+    router.put('/lucydraw', passport.authenticate('bearerPC', {session: false}), function (req, res, next) {
+        var id = req.query.id;
+        var data = req.body;
+        LucyDraw.update({_id:id},data,function (err,result) {
+            if (err) {
+                res.json({
+                    code: '500',
+                    msg: "Error: 服务器错误"
+                });
+            } else {
+                res.json({
+                    code: '0',
+                    result: result
+                });
+            }
+        });
     });
 
 };
