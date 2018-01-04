a07a36fb572de908a9d4d765756d8a80119d8a31
Administrator <cunkuan.ding@tenbent.com>
2018-01-04 16:06:33 +0800

    抽奖上下架

--- routes/pc/lucydraw.js
+++ routes/pc/lucydraw.js
@@ -70,11 +70,34 @@ exports.pcRouter = function (router) {
         });
     });
 
-
+    /**
+     * 抽奖编辑
+     */
     router.put('/lucydraw', passport.authenticate('bearerPC', {session: false}), function (req, res, next) {
         var id = req.query.id;
         var data = req.body;
         LucyDraw.update({_id:id},data,function (err,result) {
+            if (err) {
+                res.json({
+                    code: '500',
+                    msg: "Error: 服务器错误"
+                });
+            } else {
+                res.json({
+                    code: '0',
+                    result: result
+                });
+            }
+        });
+    });
+
+    /**
+     * 抽奖上下架
+     */
+    router.put('/lucydraw_effect', passport.authenticate('bearerPC', {session: false}), function (req, res, next) {
+        var id = req.query.id;
+        var effect = req.query.effect;
+        LucyDraw.update({_id:id},{effect:effect},function (err,result) {
             if (err) {
                 res.json({
                     code: '500',
